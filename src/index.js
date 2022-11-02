822b14cc7a0892c5b4f03a137ab10fda5ed3c605
Agustin Sampayo <agustines82@gmail.com>
2022-11-02 12:07:03 -0300

    conecto la base de datos

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+import recetaRouter from "./routes/recetas.routes";
+//llamar a la conecion a la bd (ejecutar el archivo database)
+import "./database";
+
+//crear una instancia de express
+const app = express();
+//configurar un puerto
+app.set("port", process.env.PORT || 4001);
+
+app.listen(app.get("port"), () => {
+    console.log("estoy en el puerto " + app.get("port"));
+});
+
+//midlewares: funciones que se ejecutan antes de las rutas
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); //interpreta el formato json
+app.use(express.urlencoded({ extends: true })); // permite extraer la peticion post que viene en json
+app.use(morgan("dev"));
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname, "../public")));
+
+//rutas
+app.use("/apirecetas", recetaRouter);
